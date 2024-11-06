b173de12960c5fa8bee92f16704f5c8a1ac851d9
St. John Johnson <st.john.johnson@gmail.com>
2024-11-05 17:51:15 -0800

    Adding the new modified code to read from Extra-Life.org and count up/down from a specific datetime.

--- README.md
+++ README.md
@@ -1,8 +1,72 @@
 # extralife-matrixportal-tracker
 Tracking donations for ExtraLife on a 32x64 RGB Matrix via MatrixPortal
 
+## Features
+
+This tracker will display the number of hours, minutes, and seconds until your 24 hour game day event.  Once it hits that time, it will begin to count up so you can track how long you've been gaming for.
+
+Additionally, it will show the current donation total.
+
+## Installation
+
+1. Follow [this guide](https://learn.adafruit.com/network-connected-metro-rgb-matrix-clock/code-the-matrix-clock) to install CircuitPython on the MatrixPortal.
+2. Add the following libraries:
+ - `adafruit_bitmap_font`
+ - `adafruit_bus_device`
+ - `adafruit_display_text`
+ - `adafruit_esp32spi`
+ - `adafruit_io`
+ - `adafruit_matrixportal`
+ - `adafruit_portalbase`
+ - `adafruit_connection_manager.mpy`
+ - `adafruit_datetime.mpy`
+ - `adafruit_debouncer.mpy`
+ - `adafruit_fakerequests.mpy`
+ - `adafruit_lis3dh.mpy`
+ - `adafruit_requests.mpy`
+ - `neopixel.mpy`
+3. Create your `secrets.py` file as listed below.
+4. Copy the `code.py` and `Micro5-Regular-21.bdf` files.
+5. Enjoy!
+
+## Configuration
+
+Create a `secrets.py` file with the following contents:
+```python
+secrets = {
+    # WiFi Network Configuration
+    'ssid': '_your_ssid_',
+    'password': '_your_wifi_password_',
+
+    # AIO API Config for local time
+    'aio_username': '_your_aio_username_',
+    'aio_key': '_your_aio_key_',
+    
+    # ExtraLife Participant ID from
+    # https://www.extra-life.org/index.cfm?fuseaction=donordrive.participant&participantID=_your_id_
+    'extralife_id': '_your_participant_id_',
+    # ISO8601 formatted UTC time for your game day event
+    'target_date': '2024-11-02T17:30:00',
+}
+```
+
+### Optional Settings
+
+```python
+{
+    # How many seconds between ExtraLife checks
+    'refresh_frequency': 30,
+    # What ExtraLife server to use
+    'extralife_server': 'https://www.extra-life.org',
+}
+```
+
 ## Inspiration
 
 Based on the Metro_Matrix_Clock from John Park at Adafruit Industries (MIT License)
  - [Source code](https://github.com/adafruit/Adafruit_Learning_System_Guides/blob/main/Metro_Matrix_Clock/code.py)
  - [Guide](https://learn.adafruit.com/network-connected-metro-rgb-matrix-clock/overview)
+
+## Font
+
+I've bundled a slightly modified version of the [Micro 5 font](https://fonts.google.com/specimen/Micro+5).  I've specified a specific font size, added an extra pixel column to the `1` to ensure they are monospaced.
--- code.py
+++ code.py
@@ -1,9 +1,6 @@
-# SPDX-FileCopyrightText: 2020 John Park for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-# Metro Matrix Clock
-# Runs on Airlift Metro M4 with 64x32 RGB Matrix display & shield
+# ExtraLife MatrixPortal Display
+# Author: St. John Johnson <st.john.johnson@gmail.com>
+# Based on John Park's Metro Matrix Clock
 
 import time
 import board
@@ -13,32 +10,53 @@ from adafruit_display_text.label import Label
 from adafruit_bitmap_font import bitmap_font
 from adafruit_matrixportal.network import Network
 from adafruit_matrixportal.matrix import Matrix
+from adafruit_matrixportal.matrixportal import MatrixPortal
+from adafruit_datetime import datetime
 
-BLINK = True
 DEBUG = False
 
-# Get wifi details and more from a secrets.py file
+# Get configuration settings from a secrets.py file
+target_date = None
+extralife_id = None
+extralife_server = 'https://www.extra-life.org'
+refresh_frequency = 30
+
 try:
-    from secrets import secrets
+    from secrets import secrets # type: ignore
+
+    if "target_date" in secrets:
+        target_date = datetime.fromisoformat(secrets["target_date"])
+    if "extralife_id" in secrets:
+        extralife_id = secrets["extralife_id"]
+    if "extralife_server" in secrets:
+        extralife_server = secrets["extralife_server"]
+    if "refresh_frequency" in secrets:
+        refresh_frequency = secrets["refresh_frequency"]
+
 except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
+    print("Configuration settings are kept in secrets.py, please add them there!")
     raise
-print("    Metro Minimal Clock")
-print("Time will be set for {}".format(secrets["timezone"]))
+
+print("Extra Life Countdown and Tracker")
+print("Target Date: {}".format(target_date.isoformat()))
+print("Extra Life ID: {}".format(extralife_id))
+print("Extra Life Server: {}".format(extralife_server))
+print("Refresh Frequency: {}".format(refresh_frequency))
 
 # --- Display setup ---
-matrix = Matrix()
+matrix = MatrixPortal(
+    status_neopixel=board.NEOPIXEL,
+    debug=DEBUG
+)
 display = matrix.display
-network = Network(status_neopixel=board.NEOPIXEL, debug=False)
 
 # --- Drawing setup ---
 group = displayio.Group()  # Create a Group
 bitmap = displayio.Bitmap(64, 32, 2)  # Create a bitmap object,width, height, bit depth
 color = displayio.Palette(4)  # Create a color palette
 color[0] = 0x000000  # black background
-color[1] = 0xFF0000  # red
-color[2] = 0xCC4000  # amber
-color[3] = 0x85FF00  # greenish
+color[1] = 0x1AC1DD  # teal
+color[2] = 0x85FF00  # greenish
 
 # Create a TileGrid using the Bitmap and Palette
 tile_grid = displayio.TileGrid(bitmap, pixel_shader=color)
@@ -46,60 +64,110 @@ group.append(tile_grid)  # Add the TileGrid to the Group
 display.root_group = group
 
 if not DEBUG:
-    font = bitmap_font.load_font("/IBMPlexMono-Medium-24_jep.bdf")
+    font = bitmap_font.load_font("/Micro5-Regular-21.bdf") 
 else:
     font = terminalio.FONT
 
-clock_label = Label(font)
+clock_label = Label(font, background_tight=True)
+clock_label.color = color[1]
+money_label = Label(font, background_tight=True)
+money_label.color = color[2]
+
+def time_until_target(target_date):
+    # Get the current UTC time
+    current_time = datetime.now()
+    
+    # Calculate the difference
+    time_difference = target_date - current_time
+    if time_difference.days < 0:
+        time_difference = current_time - target_date
+    
+    # Extract hours, minutes, and seconds
+    seconds = time_difference.seconds
+    hours = (time_difference.days * 24) + (seconds // 3600)
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    
+    return hours, minutes, seconds
+
+def format_dollars(amount):
+    # Convert the float to an integer
+    amount_int = int(amount)
+    # Convert the integer to a string with commas
+    formatted = "${:,}".format(amount_int)
+    return formatted
 
 
-def update_time(*, hours=None, minutes=None, show_colon=False):
-    now = time.localtime()  # Get the time values we need
-    if hours is None:
-        hours = now[3]
-    if hours >= 18 or hours < 6:  # evening hours to morning
-        clock_label.color = color[1]
-    else:
-        clock_label.color = color[3]  # daylight hours
-    if hours > 12:  # Handle times later than 12:59
-        hours -= 12
-    elif not hours:  # Handle times between 0:00 and 0:59
-        hours = 12
+def money_raised():
+    print("Fetching text from")
+    data = matrix.network.fetch(
+        url="{}/api/participants/{}".format(extralife_server, extralife_id)
+    ).json()
+    donation_total = data["sumDonations"]
+    donation_goal = data["fundraisingGoal"]
+    donation_percent = donation_total / donation_goal * 100
 
-    if minutes is None:
-        minutes = now[4]
+    return format_dollars(donation_total), format_dollars(donation_goal), donation_percent
 
-    if BLINK:
-        colon = ":" if show_colon or now[5] % 2 else " "
-    else:
-        colon = ":"
+def update_donation():
+    total, goal, percent = money_raised()
+    
+    money_label.text = "{total}".format(
+        total=total
+    )
+    bbx, bby, bbwidth, bbh = money_label.bounding_box
 
-    clock_label.text = "{hours}{colon}{minutes:02d}".format(
-        hours=hours, minutes=minutes, colon=colon
+    # Center the label
+    money_label.x = round(display.width / 2 - bbwidth / 2) + 1
+    money_label.y = display.height - (bbh // 2) - 3
+
+    if DEBUG:
+        print("Money Label bounding box: {},{},{},{}".format(bbx, bby, bbwidth, bbh))
+        print("Money Label x: {} y: {}".format(money_label.x, money_label.y))
+
+def update_time(*, hours=None, minutes=None, seconds=None, show_colon=False):
+    hours, minutes, seconds = time_until_target(target_date)
+
+    clock_label.text = "{hours}:{minutes:02d}:{seconds:02d}".format(
+        hours=hours, minutes=minutes, seconds=seconds
     )
     bbx, bby, bbwidth, bbh = clock_label.bounding_box
-    # Center the label
-    clock_label.x = round(display.width / 2 - bbwidth / 2)
-    clock_label.y = display.height // 2
-    if DEBUG:
-        print("Label bounding box: {},{},{},{}".format(bbx, bby, bbwidth, bbh))
-        print("Label x: {} y: {}".format(clock_label.x, clock_label.y))
 
+    # Center the label
+    clock_label.x = round(display.width / 2 - bbwidth / 2) + 1
+    clock_label.y = bbh // 2
+    if DEBUG:
+        print("Clock Label bounding box: {},{},{},{}".format(bbx, bby, bbwidth, bbh))
+        print("Clock Label x: {} y: {}".format(clock_label.x, clock_label.y))
 
 last_check = None
-update_time(show_colon=True)  # Display whatever time is on the board
-group.append(clock_label)  # add the clock label to the group
+last_sync = None
+# bootstrap the internet clock
+matrix.get_local_time("UTC")
+# get the latest data
+update_time()
+update_donation()
+# add the clock label to the group
+group.append(clock_label)  
+group.append(money_label)
 
 while True:
-    if last_check is None or time.monotonic() > last_check + 3600:
+    # Check for donations every X seconds
+    if last_check is None or time.monotonic() > last_check + refresh_frequency:
         try:
-            update_time(
-                show_colon=True
-            )  # Make sure a colon is displayed while updating
-            network.get_local_time()  # Synchronize Board's clock to Internet
+            update_donation()
             last_check = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+
+    # Sync time every 30 minutes
+    if last_sync is None or time.monotonic() > last_sync + 1800:
+        try:
+            matrix.get_local_time("UTC")  # Synchronize Board's clock to Internet
+            last_sync = time.monotonic()
         except RuntimeError as e:
             print("Some error occured, retrying! -", e)
 
     update_time()
     time.sleep(1)
+ # type: ignore
